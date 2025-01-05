b3c1e20ee5c0274bdd05b6153f9b25d3bca20fa0
Marc Jenald Galino <galinomarcjenald20@gmail.com>
2025-01-05 16:48:20 +0800

    Register user as admin by default and update the test

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Models\User;
+use App\RolesEnum;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
@@ -41,6 +42,9 @@ class RegisteredUserController extends Controller
             'email' => $request->email,
             'password' => Hash::make($request->password),
         ]);
+
+        // Register user as admin role by default
+        $user->assignRole(RolesEnum::ADMIN);
 
         event(new Registered($user));
 
--- tests/Feature/Auth/RegistrationTest.php
+++ tests/Feature/Auth/RegistrationTest.php
@@ -1,18 +1,30 @@
 <?php
 
+use App\Models\User;
+use App\RolesEnum;
+use Database\Seeders\RoleAndPermissionSeeder;
+
 test('registration screen can be rendered', function () {
     $response = $this->get('/register');
 
     $response->assertStatus(200);
 });
 
-test('new users can register', function () {
+test('new users can register and have admin role by default', function () {
+    $this->artisan('db:seed', ['--class' => RoleAndPermissionSeeder::class]);
+
     $response = $this->post('/register', [
         'name' => 'Test User',
         'email' => 'test@example.com',
         'password' => 'password',
         'password_confirmation' => 'password',
     ]);
+
+    // Fetch the newly created user
+    $user = User::where('email', 'test@example.com')->first();
+
+    // Assert the user has been assigned the default role
+    expect($user->hasRole(RolesEnum::ADMIN))->toBeTrue();
 
     $this->assertAuthenticated();
     $response->assertRedirect(route('dashboard', absolute: false));
